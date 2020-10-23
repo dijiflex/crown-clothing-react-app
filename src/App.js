a733b28b49b096bc8ef113f400fc4fdf45e9cdee
dijiflex <dijiflex@gmail.com>
2020-10-23 13:29:31 +0300

    added header component and link to the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 import './App.css';
 
@@ -11,10 +12,15 @@ import './App.css';
 
 function App() {
     return (
-        <div >
+
+        <div>
+            <Header />
+            <Switch>
             <Route exact path='/' component={Homepage} />
             <Route exact path='/shop' component={ShopPage} />
+        </Switch>
         </div>
+
     );
 }
 

c1f7dfada6a7e62a3337d138331f4cbc31020625
dijiflex <dijiflex@gmail.com>
2020-11-09 15:18:15 +0300

    added button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
+import  SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component'
 
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
             <Switch>
             <Route exact path='/' component={Homepage} />
             <Route exact path='/shop' component={ShopPage} />
+            <Route exact path='/signin' component={SignInAndSignUpPage} />
         </Switch>
         </div>
 

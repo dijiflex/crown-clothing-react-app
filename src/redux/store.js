0b09ae2596433be0dd450e30a2c72bed23eb0642
dijiflex <dijiflex@gmail.com>
2021-02-03 21:49:18 +0300

    removed unwanted spaces

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,6 @@ import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
-
 import rootReducer from './root-reducer';
 
 const middlewares = [];

1938c56f23b996be0dfc922aa01e785aae14c319
dijiflex <dijiflex@gmail.com>
2021-02-04 22:27:59 +0300

    Modified store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
-
+///kljdfkjds
 import rootReducer from './root-reducer';
 
 const middlewares = [];
